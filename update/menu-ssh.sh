29d891eb3add780ce84c94a381c6393b9f37c873
titis69 <117496052+titis69@users.noreply.github.com>
2024-09-25 22:54:13 +0700

    Update menu-ssh.sh

--- update/menu-ssh.sh
+++ update/menu-ssh.sh
@@ -8,11 +8,7 @@ RED="\033[0;31m"
 COLOR1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
 COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
 ###########- END COLOR CODE -##########
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
 
 function addssh(){
 clear
